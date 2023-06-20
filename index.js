2d115d9d24f4cae2a8f6a6cf3379aadbb41201da
Danilo <danilo25aciole@gmail.com>
2023-06-20 16:52:51 -0300

    volta aos trilho 4

--- index.js
+++ index.js
@@ -5,16 +5,73 @@ var multer = require('multer');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: '/tmp/'}));
+app.use(bodyParser.json());
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/');
+  }
+});
+
+var upload = multer({ storage: storage });
+
+var tasks = []; // Array para armazenar as tarefas
+var taskIdCounter = 1; // Contador incremental para gerar IDs
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/tarefas.html');
-})
+});
 
+// Rota para obter as tarefas existentes
+app.get('/api/tasks', function (req, res) {
+  console.log('Quero as tarefas...');
+  res.json(tasks);
+  console.log('Tarefas enviadas!');
+});
+
+// Rota para adicionar uma nova tarefa
+app.post('/api/tasks', function (req, res) {
+  var task = {
+    id: taskIdCounter++, // Incrementa o contador e utiliza como ID
+    text: req.body.text,
+    description: req.body.description,
+    completed: false
+  };
+  tasks.push(task);
+  res.json(task);
+});
+
+// Rota para excluir uma tarefa pelo ID
+app.delete('/api/tasks/:id', function (req, res) {
+  var taskId = parseInt(req.params.id);
+  var index = tasks.findIndex(function (task) {
+    return task.id === taskId;
+  });
+  if (index !== -1) {
+    var deletedTask = tasks.splice(index, 1);
+    res.json(deletedTask);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// Rota para atualizar o status de conclusão de uma tarefa pelo ID
+app.put('/api/tasks/:id', function (req, res) {
+  var taskId = parseInt(req.params.id);
+  var completed = req.body.completed;
+  var task = tasks.find(function (task) {
+    return task.id === taskId;
+  });
+  if (task) {
+    task.completed = completed;
+    res.json(task);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 var server = app.listen(8081, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Exemplo escutando em http://%s:%s", host,
-    port)
-})
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Exemplo escutando em http://%s:%s", host, port);
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,39 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Carregar as tarefas existentes do arquivo JSON
   var tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
-  
+
   // Variáveis para a manipulação da tela modal
   var addTaskModal = document.getElementById('add-task-modal');
   var modal = document.getElementById('modal');
-  
+
   var titleT = document.getElementById('task-title');
   var taskDescription = document.getElementById('task-description');
-  
+
   var addTaskBtn = document.getElementById('add-task-btn');
   var saveTaskBtn = document.getElementById('save-task-btn');
   var closeAddTaskBtn = document.getElementById('cancel-task-btn');
-  
+
   var deleteTaskBtn = document.getElementById('delete-task');
   var completeTaskBtn = document.getElementById('complete-task');
   var closeTaskBtn = document.getElementById('close-task');
-  
+
   var selectedTaskIndex = -1; // Índice da tarefa selecionada
-  
+
   // Função para atualizar a exibição das tarefas na página
   function renderTasks() {
     var taskList = document.getElementById('task-list');
     taskList.innerHTML = '';
-  
+
     tasks.forEach(function (task, index) {
       var taskItem = document.createElement('li');
       var taskText = document.createElement('span');
       taskText.innerText = task.text;
-  
+
       // Marcar como concluída
       if (task.completed) {
         taskText.classList.add('completed');
       }
-  
+
       // Abrir a tela modal ao clicar na tarefa
       taskText.addEventListener('click', function () {
         if (!modal.classList.contains('show')) {
@@ -41,26 +41,45 @@ document.addEventListener('DOMContentLoaded', function() {
           openModal();
         }
       });
-  
+
       taskItem.appendChild(taskText);
       taskList.appendChild(taskItem);
     });
   }
-  //Abrir tela de adicionar tarefas
+
+  // Abrir tela de adicionar tarefas
   function openAddTaskModal() {
     addTaskModal.classList.add('show');
+
+    var descriptionInput = document.getElementById('new-task-description');
+    var descriptionLimit = document.getElementById('description-limit');
+    var caracteres = 150 - descriptionInput.value.length;
+    descriptionLimit.textContent = 'Caracteres restantes: ' + caracteres;
+
+    // Evento de input para atualizar o contador de caracteres
+    descriptionInput.addEventListener('input', function () {
+      var caracteres = 150 - descriptionInput.value.length;
+      descriptionLimit.textContent = 'Caracteres restantes: ' + caracteres;
+    });
   }
-  
+
+  saveTaskBtn.addEventListener('click', function () {
+    addTask();
+  });
+  closeAddTaskBtn.addEventListener('click', function () {
+    addTaskModal.classList.remove('show');
+  });
+
   // Função para fechar a tela de adicionar tarefas
   function closeAddTaskModal() {
     addTaskModal.classList.remove('show');
   }
-  
+
   // Evento de clique no botão "Adicionar Tarefa"
   addTaskBtn.addEventListener('click', function () {
-    addTask();
-  })
-  
+    openAddTaskModal();
+  });
+
   // Função para abrir a tela modal
   function openModal() {
     if (selectedTaskIndex !== -1) {
@@ -69,8 +88,7 @@ document.addEventListener('DOMContentLoaded', function() {
       titleT.innerHTML = task.text;
       taskDescription.innerHTML = task.description;
     }
-  
-  
+
     // Verificar se a tarefa está concluída
     if (task.completed) {
       completeTaskBtn.textContent = 'Desmarcar';
@@ -78,92 +96,145 @@ document.addEventListener('DOMContentLoaded', function() {
       completeTaskBtn.textContent = 'Concluir';
     }
   }
-  
+
   // Função para fechar a tela modal
   function closeModal() {
     modal.classList.remove('show');
     selectedTaskIndex = -1; // Limpa o índice da tarefa selecionada
   }
-  
+
   // Ação de excluir tarefa
   deleteTaskBtn.addEventListener('click', function () {
     if (selectedTaskIndex !== -1) {
-      tasks.splice(selectedTaskIndex, 1);
-      saveTasks();
-      renderTasks();
-      closeModal();
+      var taskId = tasks[selectedTaskIndex].id;
+      deleteTask(taskId);
     }
   });
-  
+
   // Ação de marcar ou desmarcar tarefa como concluída
   completeTaskBtn.addEventListener('click', function () {
     if (selectedTaskIndex !== -1) {
       var task = tasks[selectedTaskIndex];
-      task.completed = !task.completed;
-      saveTasks();
-      renderTasks();
-      closeModal();
+      var taskId = task.id;
+      var completed = !task.completed;
+      updateTaskCompletion(taskId, completed);
     }
   });
-  
+
   // Ação de fechar a tela modal
   closeTaskBtn.addEventListener('click', function () {
     closeModal();
   });
-  
-  // Função para adicionar uma nova tarefa
+
   // Função para adicionar uma nova tarefa
   function addTask() {
-    var caracteres = 150;
     var taskInput = document.getElementById('new-task-input');
     var taskText = taskInput.value.trim();
     var descriptionInput = document.getElementById('new-task-description');
     var descriptionText = descriptionInput.value.trim();
-  
+
     if (taskText !== '') {
       // Verificar limite de caracteres da descrição
       if (descriptionText.length > 150) {
         alert('A descrição deve ter no máximo 150 caracteres.');
         return;
       }
-  
-      tasks.push({ text: taskText, description: descriptionText, completed: false });
-      saveTasks();
-      taskInput.value = '';
-      descriptionInput.value = '';
-      renderTasks();
-      closeAddTaskModal(); // Fechar o modal após adicionar a tarefa
+
+      var task = { text: taskText, description: descriptionText, completed: false };
+
+      fetch('/api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+      })
+        .then(function (response) {
+          console.log('Pegue isso:', task);
+          if (response.ok) {
+            return response.json();
+          } else {
+            console.log('Erro ao adicionar a tarefa:', response.status);
+          }
+        })
+        .then(function (data) {
+          if (data) {
+            task.id = data.id;
+            tasks.push(task);
+            saveTasks();
+            taskInput.value = '';
+            descriptionInput.value = '';
+            renderTasks();
+            closeAddTaskModal(); // Fechar o modal após adicionar a tarefa
+            console.log('Tarefa adicionada com sucesso!');
+          }
+        })
+        .catch(function (error) {
+          console.log('Erro ao adicionar a tarefa:', error);
+        });
     }
   }
-  
-  // Evento de input na descrição da tarefa para atualizar o contador de caracteres restantes
-  document.getElementById('new-task-description').addEventListener('input', function () {
-    var descriptionInput = document.getElementById('new-task-description');
-    var descriptionLimit = document.getElementById('description-limit');
-    caracteres = 150 - descriptionInput.value.length;
-    descriptionLimit.textContent = 'Caracteres restantes: ' + caracteres;
-  });
-  
-  // Evento de clique no botão "Adicionar Tarefa"
-  addTaskBtn.addEventListener('click', function () {
-    openAddTaskModal();
-  });
-  
-  // Evento de clique no botão "Salvar" dentro do modal de adicionar tarefas
-  saveTaskBtn.addEventListener('click', function () {
-    addTask();
-  });
-  
-  // Evento de clique no botão "Cancelar" dentro do modal de adicionar tarefas
-  closeAddTaskBtn.addEventListener('click', function () {
-    closeAddTaskModal();
-  });
-  
+
+  // Função para excluir uma tarefa
+  function deleteTask(taskId) {
+    fetch(`/api/tasks/${taskId}`, {
+      method: 'DELETE'
+    })
+      .then(function (response) {
+        console.log('Quero excluir a tarefa com ID:', taskId);
+        if (response.ok) {
+          tasks = tasks.filter(function (task) {
+            return task.id !== taskId;
+          });
+          saveTasks();
+          renderTasks();
+          closeModal();
+          console.log('Tarefa excluída com sucesso!');
+        } else {
+          console.log('Erro ao excluir a tarefa:', response.status);
+        }
+      })
+      .catch(function (error) {
+        console.log('Erro ao excluir a tarefa:', error);
+      });
+  }
+
+  // Função para atualizar o status de conclusão de uma tarefa
+  function updateTaskCompletion(taskId, completed) {
+    fetch(`/api/tasks/${taskId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ completed: completed })
+    })
+      .then(function (response) {
+        console.log('Quero atualizar o status de conclusão da tarefa com ID:', taskId);
+        if (response.ok) {
+          var task = tasks.find(function (task) {
+            return task.id === taskId;
+          });
+          if (task) {
+            task.completed = completed;
+            saveTasks();
+            renderTasks();
+            closeModal();
+            console.log('Status de conclusão da tarefa atualizado com sucesso!');
+          }
+        } else {
+          console.log('Erro ao atualizar o status de conclusão da tarefa:', response.status);
+        }
+      })
+      .catch(function (error) {
+        console.log('Erro ao atualizar o status de conclusão da tarefa:', error);
+      });
+  }
+
   // Função para salvar as tarefas no arquivo JSON
   function saveTasks() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }
-  
+
   // Renderizar as tarefas na página ao carregar
   renderTasks();
-  });
+});
