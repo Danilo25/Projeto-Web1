d47d1aae9ba8871981360c32c3810e373313350b
Danilo <danilo25aciole@gmail.com>
2023-06-26 09:38:30 -0300

    ok

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ var upload = multer({ storage: storage });
 
 var tasksFile = 'tarefas.json'; // Nome do arquivo JSON para armazenar as tarefas
 
-// Função para ler as tarefas do arquivo JSON
 function readTasksFromFile() {
   try {
     var data = fs.readFileSync(tasksFile, 'utf8');
@@ -29,7 +28,6 @@ function readTasksFromFile() {
   }
 }
 
-// Função para salvar as tarefas no arquivo JSON
 function saveTasksToFile(tasks) {
   try {
     var data = JSON.stringify(tasks);
@@ -40,35 +38,29 @@ function saveTasksToFile(tasks) {
   }
 }
 
-var tasks = readTasksFromFile(); // Carrega as tarefas do arquivo JSON
-var taskIdCounter = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1; // Obtém o próximo ID com base nas tarefas existentes
+var tasks = readTasksFromFile();
+var taskIdCounter = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
 
 app.get('/', function (req, res) {
-  var tasksFromFile = readTasksFromFile(); // Lê as tarefas do arquivo JSON
   res.sendFile(__dirname + '/public/tarefas.html');
 });
 
-// Rota para obter as tarefas existentes
 app.get('/api/tasks', function (req, res) {
-  console.log('Quero as tarefas...');
   res.json(tasks);
-  console.log('Tarefas enviadas!');
 });
 
-// Rota para adicionar uma nova tarefa
 app.post('/api/tasks', function (req, res) {
   var task = {
-    id: taskIdCounter++, // Incrementa o contador e utiliza como ID
+    id: taskIdCounter++,
     text: req.body.text,
     description: req.body.description,
     completed: false
   };
   tasks.push(task);
-  saveTasksToFile(tasks); // Salva as tarefas no arquivo JSON
+  saveTasksToFile(tasks);
   res.json(task);
 });
 
-// Rota para excluir uma tarefa pelo ID
 app.delete('/api/tasks/:id', function (req, res) {
   var taskId = parseInt(req.params.id);
   var index = tasks.findIndex(function (task) {
@@ -76,14 +68,13 @@ app.delete('/api/tasks/:id', function (req, res) {
   });
   if (index !== -1) {
     var deletedTask = tasks.splice(index, 1);
-    saveTasksToFile(tasks); // Salva as tarefas no arquivo JSON
+    saveTasksToFile(tasks);
     res.json(deletedTask);
   } else {
     res.sendStatus(404);
   }
 });
 
-// Rota para atualizar o status de conclusão de uma tarefa pelo ID
 app.put('/api/tasks/:id', function (req, res) {
   var taskId = parseInt(req.params.id);
   var completed = req.body.completed;
@@ -92,26 +83,13 @@ app.put('/api/tasks/:id', function (req, res) {
   });
   if (task) {
     task.completed = completed;
-    saveTasksToFile(tasks); // Salva as tarefas no arquivo JSON
+    saveTasksToFile(tasks);
     res.json(task);
   } else {
     res.sendStatus(404);
   }
 });
-// Rota para visualizar o conteúdo do arquivo tarefas.json
-app.get('/api/tasks/file', function (req, res) {
-  try {
-    var data = fs.readFileSync(tasksFile, 'utf8');
-    var tasksFromFile = JSON.parse(data);
-    res.json(tasksFromFile);
-  } catch (err) {
-    console.log('Erro ao ler o arquivo de tarefas:', err);
-    res.sendStatus(500);
-  }
-});
 
 var server = app.listen(8080, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("Exemplo escutando em http://localhost:%s", port);
+  console.log("Exemplo escutando em http://localhost:%s", server.address().port);
 });
